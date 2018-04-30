36823b2cc05cc64a0e6e0457f4ac14f051d1ad38
huangweiqi <askviky2010@gmail.com>
2018-04-30 12:25:47 +0800

    实现开始和停止硬编码

---
+++ app/src/main/java/com/abt/media/AvcEncoder.java
@@ -0,0 +1,182 @@
+package com.abt.media;
+
+import android.annotation.SuppressLint;
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
+import android.os.Environment;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+/**
+ * Created by huangweiqi on 29/04/2018.
+ */
+public class AvcEncoder {
+
+    private final static String TAG = "AvcEncoder";
+    private int TIMEOUT_USE_SECOND = 12000;
+    private MediaCodec mMediaCodec;
+    private int mWidth;
+    private int mHeight;
+    private int mFrameRate;
+    private byte[] mInfo = null;
+    public byte[] mConfigByte;
+    private static String mPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/test1.h264";
+    private BufferedOutputStream mOutputStream;
+    private FileOutputStream mOutStream;
+    private ByteBuffer[] mInputBuffers;
+    private ByteBuffer[] mOutputBuffers;
+    public boolean mIsRuning = false;
+    private int mCount = 0;
+
+    @SuppressLint("NewApi")
+    public AvcEncoder(int width, int height, int framerate, int bitrate) {
+        mWidth = width;
+        mHeight = height;
+        mFrameRate = framerate;
+        MediaFormat mediaFormat = MediaFormat.createVideoFormat("video/avc", width, height);
+        mediaFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420SemiPlanar);
+        mediaFormat.setInteger(MediaFormat.KEY_BIT_RATE, width*height*5);
+        mediaFormat.setInteger(MediaFormat.KEY_FRAME_RATE, 30);
+        mediaFormat.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 1);
+        try {
+            mMediaCodec = MediaCodec.createEncoderByType("video/avc");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mMediaCodec.configure(mediaFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+        mMediaCodec.start();
+        createFile();
+    }
+
+    private void createFile() {
+        File file = new File(mPath);
+        if(file.exists()){
+            file.delete();
+        }
+        try {
+            mOutputStream = new BufferedOutputStream(new FileOutputStream(file));
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressLint("NewApi")
+    private void stopEncoder() {
+        try {
+            mMediaCodec.stop();
+            mMediaCodec.release();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void stopThread(){
+        mIsRuning = false;
+        try {
+            stopEncoder();
+            mOutputStream.flush();
+            mOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startEncoderThread() {
+        Thread encoderThread = new Thread(new Runnable() {
+
+            @SuppressLint("NewApi")
+            @Override
+            public void run() {
+                mIsRuning = true;
+                byte[] input = null;
+                long pts =  0;
+                long generateIndex = 0;
+
+                while (mIsRuning) {
+                    if (MainActivity.mYUVQueue.size() >0){
+                        input = MainActivity.mYUVQueue.poll();
+                        byte[] yuv420sp = new byte[mWidth * mHeight *3/2];
+                        NV21ToNV12(input,yuv420sp, mWidth, mHeight);
+                        input = yuv420sp;
+                    }
+                    if (input != null) {
+                        try {
+                            long startMs = System.currentTimeMillis();
+                            ByteBuffer[] inputBuffers = mMediaCodec.getInputBuffers();
+                            ByteBuffer[] outputBuffers = mMediaCodec.getOutputBuffers();
+                            int inputBufferIndex = mMediaCodec.dequeueInputBuffer(-1);
+                            if (inputBufferIndex >= 0) {
+                                pts = computePresentationTime(generateIndex);
+                                ByteBuffer inputBuffer = inputBuffers[inputBufferIndex];
+                                inputBuffer.clear();
+                                inputBuffer.put(input);
+                                mMediaCodec.queueInputBuffer(inputBufferIndex, 0, input.length, pts, 0);
+                                generateIndex += 1;
+                            }
+
+                            MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+                            int outputBufferIndex = mMediaCodec.dequeueOutputBuffer(bufferInfo, TIMEOUT_USE_SECOND);
+                            while (outputBufferIndex >= 0) {
+                                //Log.i("AvcEncoder", "Get H264 Buffer Success! flag = "+bufferInfo.flags+",pts = "+bufferInfo.presentationTimeUs+"");
+                                ByteBuffer outputBuffer = outputBuffers[outputBufferIndex];
+                                byte[] outData = new byte[bufferInfo.size];
+                                outputBuffer.get(outData);
+                                if(bufferInfo.flags == 2){
+                                    mConfigByte = new byte[bufferInfo.size];
+                                    mConfigByte = outData;
+                                }else if(bufferInfo.flags == 1){
+                                    byte[] keyframe = new byte[bufferInfo.size + mConfigByte.length];
+                                    System.arraycopy(mConfigByte, 0, keyframe, 0, mConfigByte.length);
+                                    System.arraycopy(outData, 0, keyframe, mConfigByte.length, outData.length);
+                                    mOutputStream.write(keyframe, 0, keyframe.length);
+                                }else{
+                                    mOutputStream.write(outData, 0, outData.length);
+                                }
+
+                                mMediaCodec.releaseOutputBuffer(outputBufferIndex, false);
+                                outputBufferIndex = mMediaCodec.dequeueOutputBuffer(bufferInfo, TIMEOUT_USE_SECOND);
+                            }
+                        } catch (Throwable t) {
+                            t.printStackTrace();
+                        }
+                    } else {
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+        encoderThread.start();
+    }
+
+    private void NV21ToNV12(byte[] nv21,byte[] nv12,int width,int height) {
+        if(nv21 == null || nv12 == null)return;
+        int framesize = width*height;
+        int i = 0,j = 0;
+        System.arraycopy(nv21, 0, nv12, 0, framesize);
+        for(i = 0; i < framesize; i++){
+            nv12[i] = nv21[i];
+        }
+        for (j = 0; j < framesize/2; j+=2) {
+            nv12[framesize + j-1] = nv21[j+framesize];
+        }
+        for (j = 0; j < framesize/2; j+=2) {
+            nv12[framesize + j] = nv21[j+framesize-1];
+        }
+    }
+
+    /**
+     * Generates the presentation time for frame N, in microseconds.
+     */
+    private long computePresentationTime(long frameIndex) {
+        return 132 + frameIndex * 1000000 / mFrameRate;
+    }
+}
--- app/src/main/java/com/abt/media/MainActivity.java
+++ app/src/main/java/com/abt/media/MainActivity.java
@@ -1,13 +1,141 @@
 package com.abt.media;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.graphics.ImageFormat;
+import android.hardware.Camera;
+import android.media.MediaCodecInfo;
+import android.media.MediaCodecList;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.util.concurrent.ArrayBlockingQueue;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity implements
+        SurfaceHolder.Callback, Camera.PreviewCallback {
+
+    @BindView(R.id.surface)
+    SurfaceView mSurfaceView;
+    @OnClick(R.id.button_start) void startBtn() {
+        mAvcCodec.startEncoderThread();
+        Toast.makeText(this, "start to record video", Toast.LENGTH_SHORT).show();
+    }
+    @OnClick(R.id.button_stop) void stopBtn() {
+        mAvcCodec.stopThread();
+        Toast.makeText(this, "stop to record video", Toast.LENGTH_SHORT).show();
+    }
+
+    private SurfaceHolder mSurfaceHolder;
+    private Camera mCamera;
+    private Camera.Parameters mParameters;
+    private int mWidth = 1280;
+    private int mHeight = 720;
+    private int mFrameRate = 30;
+    private int mBiteRate = 8500*1000;
+    private static int mYuvQueueSize = 10;
+    private AvcEncoder mAvcCodec;
+    public static ArrayBlockingQueue<byte[]> mYUVQueue = new ArrayBlockingQueue<byte[]>(mYuvQueueSize);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mSurfaceHolder = mSurfaceView.getHolder();
+        mSurfaceHolder.addCallback(this);
+        supportAvcCodec();
     }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        mCamera = getBackCamera();
+        startCamera(mCamera);
+        mAvcCodec = new AvcEncoder(mWidth, mHeight, mFrameRate, mBiteRate);
+        //mAvcCodec.startEncoderThread();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (null != mCamera) {
+            mCamera.setPreviewCallback(null);
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+            //mAvcCodec.stopThread();
+        }
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] data, android.hardware.Camera camera) {
+        putYUVData(data,data.length);
+    }
+
+    public void putYUVData(byte[] buffer, int length) {
+        if (mYUVQueue.size() >= 10) {
+            mYUVQueue.poll();
+        }
+        mYUVQueue.add(buffer);
+    }
+
+    @SuppressLint("NewApi")
+    private boolean supportAvcCodec() {
+        if (Build.VERSION.SDK_INT>=18) {
+            for (int j = MediaCodecList.getCodecCount() - 1; j >= 0; j--) {
+                MediaCodecInfo codecInfo = MediaCodecList.getCodecInfoAt(j);
+                String[] types = codecInfo.getSupportedTypes();
+                for (int i = 0; i < types.length; i++) {
+                    if (types[i].equalsIgnoreCase("video/avc")) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private void startCamera(Camera mCamera) {
+        if (mCamera != null) {
+            try {
+                mCamera.setPreviewCallback(this);
+                mCamera.setDisplayOrientation(90);
+                if(mParameters == null){
+                    mParameters = mCamera.getParameters();
+                }
+                mParameters = mCamera.getParameters();
+                mParameters.setPreviewFormat(ImageFormat.NV21);
+                mParameters.setPreviewSize(mWidth, mHeight);
+                mCamera.setParameters(mParameters);
+                mCamera.setPreviewDisplay(mSurfaceHolder);
+                mCamera.startPreview();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @TargetApi(9)
+    private Camera getBackCamera() {
+        Camera c = null;
+        try {
+            c = Camera.open(0); // attempt to get a Camera instance
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return c; // returns null if mCamera is unavailable
+    }
+
 }
